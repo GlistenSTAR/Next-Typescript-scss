adbb7ecaa345d2359e8dd407981ea09d8d197f03
Glisten **STAR** <75786284+GlistenSTAR@users.noreply.github.com>
2021-10-27 18:32:30 +0200

    add responsive navbar item

--- components/Navbar/ResponsiveModal.tsx
+++ components/Navbar/ResponsiveModal.tsx
@@ -4,51 +4,71 @@ import Image from 'next/image'
 
 import Logo from '../../assets/img/logo.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCompressArrowsAlt } from "@fortawesome/free-solid-svg-icons";
-
+import { faCompressArrowsAlt, faAngleDown } from "@fortawesome/free-solid-svg-icons";
 
 import styles from './modal.module.scss'
 
 const customStyles = {
   content: {
-    top: '8%',
+    top: '3%',
     left: '50%',
     right: 'auto',
     width:'95%',
     bottom: 'auto',
     marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    transform: 'translate(-50%, 0)',
   },
 };
 
 const ResponsiveModal = ({show, close}) => {
   return (
     <>
-    <Modal
-      isOpen={show}
-      style={customStyles}
-      contentLabel="Example Modal"
-    >
-      <div className={styles.modal} >
-        <a className="navbar-brand" href="/">
-          <Image 
-            src={Logo}
-            alt="Logo"
-            width="108"
-            height="20"
-          />
-        </a>
-        <div onClick={close} style={{float:'right'}}>
-          <FontAwesomeIcon icon={faCompressArrowsAlt}/>
+      <Modal
+        isOpen={show}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div className={styles.modal} >
+          <a className="navbar-brand" href="/">
+            <Image 
+              src={Logo}
+              alt="Logo"
+              width="108"
+              height="20"
+            />
+          </a>
+          <div onClick={close} style={{float:'right'}}>
+            <FontAwesomeIcon icon={faCompressArrowsAlt}/>
+          </div>
         </div>
-      </div>
 
-      <hr/>
-      
-      <div>
+        <hr/>
         
-      </div>
-    </Modal>
+        <div>
+          <ul className="nav navbar-nav">
+            <li className={styles.modal__item}><a href="https://docs.maladex.com/whitepaper.pdf">WhitePaper</a></li>
+            <li className={styles.modal__item}>
+              <FontAwesomeIcon icon={faAngleDown}/>
+              Info
+              <dl style={{padding: '5px 20px', marginBottom:'0px'}}>
+                <dd><a href="#">Tokenomics</a></dd>
+                <dd><a href="#">FAQ</a></dd>
+              </dl>
+            </li>
+            <li className={styles.modal__item}>
+              <FontAwesomeIcon icon={faAngleDown}/>
+              Pool
+              <dl style={{padding: '5px 20px', marginBottom:'0px'}}>
+                <dd><a href="#">Pools</a></dd>
+                <dd><a href="#">Distribution</a></dd>
+                <dd><a href="#">FAQ</a></dd>
+                <dd><a href="#">Formula</a></dd>
+              </dl>
+            </li>
+            <li className={styles.modal__item}><a href="#">Press</a></li>
+          </ul>
+        </div>
+      </Modal>
     </>
   );
 }
